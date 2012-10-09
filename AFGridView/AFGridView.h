eb500aa051c832f90fd2a5a451218aafb464e390
Alex Prokofiev <a.n.prokofyev@gmail.com>
2012-10-09 16:12:13 +0300

    Protocol for dataSource and delegate added.

--- AFGridView/AFGridView.h
+++ AFGridView/AFGridView.h
@@ -8,6 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol AFGridViewDataSource;
+@protocol AFGridViewDelegate;
+
 @interface AFGridView : UIView
+
+@property (nonatomic, weak) id<AFGridViewDataSource> dateSource;
+@property (nonatomic, weak) id<AFGridViewDelegate> delegate;
+
+@end
+
+
+@protocol AFGridViewDataSource <NSObject>
+
+- (NSInteger)numberOfRowsInGridView:(AFGridView *)gridView;
+- (NSInteger)numberOfColumnsInGridView:(AFGridView *)gridView;
+- (UIView *)nextViewForGridView:(AFGridView *)gridView;
+
+@end
+
+@protocol AFGridViewDelegate <NSObject>
+
+- (void)gridView:(AFGridView *)gridView didSelectCellAtIndex:(NSInteger)index;
 
 @end
